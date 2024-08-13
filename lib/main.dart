79344ce7ae51049df936027fc153843932bec223
Robin J <robin.j@commerceiq.ai>
2024-08-14 03:07:57 +0530

    Update dependencies in pubspec.yaml

--- lib/main.dart
+++ lib/main.dart
@@ -6,15 +6,10 @@ import 'package:google_ml_kit/google_ml_kit.dart';
 import 'package:tflite_flutter/tflite_flutter.dart' as tfl;
 import 'dart:io';
 import 'dart:math';
-import 'dart:io';
-import 'dart:ui' as ui;
 import 'package:image/image.dart' as img;
-import 'package:flutter/services.dart' show ByteData, rootBundle;
 import 'package:path/path.dart' as path;
 
 import 'package:tflite_flutter_helper_plus/tflite_flutter_helper_plus.dart';
-import 'package:tflite_flutter_helper_plus/tflite_flutter_helper_plus.dart'
-    as helpsss;
 import 'package:tflite_flutter/tflite_flutter.dart';
 
 void main() {
@@ -65,11 +60,10 @@ class _FaceRecognitionPageState extends State<FaceRecognitionPage> {
 
   Future<void> _getImage() async {
     final pickedFile = await picker.pickImage(source: ImageSource.gallery);
-    // final inputDetails = _interpreter.getInputTensor(0).shape;
-    // print("Model input details: $inputDetails");
 
     if (pickedFile != null) {
       setState(() {
+        croppedImages.clear();
         _image = File(pickedFile.path);
       });
       await _detectFaces();
@@ -87,7 +81,7 @@ class _FaceRecognitionPageState extends State<FaceRecognitionPage> {
 
     if (detectedFaces.isEmpty) {
       ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text("No faces detected.")),
+        const SnackBar(content: Text("No faces detected.")),
       );
     }
   }
@@ -102,10 +96,6 @@ class _FaceRecognitionPageState extends State<FaceRecognitionPage> {
       );
       final croppedImage = await _cropImage(_image!, cropRect);
       final faceEmbedding = await _getFaceEmbedding(croppedImage);
-      print("Face embedding: $faceEmbedding ${faceEmbeddings.toString()}");
-      print(
-          "slkadsf ${faceEmbeddings.toString()} ${knownEmbeddings.toString()}");
-      print("known embeddings: ${knownEmbeddings.toString()}");
 
       setState(() {
         faceEmbeddings[face.trackingId ?? faces.indexOf(face)] = faceEmbedding;
@@ -200,91 +190,20 @@ class _FaceRecognitionPageState extends State<FaceRecognitionPage> {
   }
 
   Future<List<double>> _getFaceEmbedding(File croppedImage) async {
-    print("Getting face embedding...");
     final inputImage = InputImage.fromFile(croppedImage);
-    // Preprocess the image
     final tensorImage = _preprocess(inputImage);
-    // img.Image image = img.decodeImage(croppedImage.readAsBytesSync())!;
-    // img.Image image = img.decodeImage(_preprocess(inputImage))!;
-    // final tensorImage = imageToByteListUint8(image, 160);
-
-    setState(() {
-      // tensorImages.add(tensorImage);
-    });
-
-    // final kkk = ResizeImage(imageProvider)
-    print("Preprocessing done ${tensorImage.tensorBuffer.getShape()}");
-
-    // Allocate tensors
     _interpreter.allocateTensors();
 
-    // Prepare input buffer
-    // ByteBuffer inputBuffer = tensorImage.buffer;
-    // TensorBuffer inputBuffer = TensorBufferFloat([1, 160, 160, 3]);
-    var inputShape = [1, 160, 160, 3]; // [batch_size, height, width, channels]
-    var inputBuffer = Float32List.fromList(tensorImage.buffer.asFloat32List());
-    print(
-        "wtf ${inputBuffer.length} ${tensorImage.buffer.asFloat32List().length}");
-    TensorBuffer inputTensorBuffer = TensorBufferFloat([1, 160, 160, 3]);
-    // inputTensorBuffer.loadList(inputBuffer, shape: inputShape);
-
-    // TensorBuffer inputBuffer = TensorBuffer.createFixedSize(
-    //     [1, 160, 160, 3],
-    //     TfLiteType.float32
-    //   );
-    // inputBuffer.loadBuffer(tensorImage.getTensorBuffer().buffer,
-    //     shape: inputBuffer.getShape());
-
-    // Allocate output buffer
-    // var outputBuffer = List.filled(128, 0.0).reshape([1, 128]);
-    // get output buffer in this shape [1, 512]
-    // var outputBuffer = List.filled(512, 0.0).reshape([1, 512]);
-    // Float32List outputBuffer = Float32List(512);
-    // ByteBuffer outputBuffer = outputList.buffer;
-    // TensorBuffer outputBuffer = TensorBuffer.createFixedSize(
-    //     [1, 512], tfl.TfLiteType.kTfLiteFloat32 as dynamic);
-
-    // print(inputBuffer.asInt8List());
-    // print(inputBuffer.toJS);
-
-    print(
-        "Input buffer: ${inputTensorBuffer.getShape()} okk1 aaa ${_interpreter.getInputTensor(0).shape} bbb ${_interpreter.getOutputTensor(0).shape}");
     var output = List.filled(1 * 512, 0).reshape([1, 512]);
-    // var input = List.filled(1 * 160 * 160 * 3, 0).reshape([1, 160, 160, 3]);
-    // // input[1] = tensorImage.getTensorBuffer().getShape();
-    // int cc = 0;
-    // for (int i = 0; i < 160; i++) {
-    //   for (int j = 0; j < 160; j++) {
-    //     for (int k = 0; k < 3; k++) {
-    //       if (cc >= inputBuffer.length) {
-    //         break;
-    //       }
-    //       input[0][i][j][k] = inputBuffer[cc];
-    //       cc += 1;
-    //     }
-    //   }
-    // }
     var input =
         tensorImage.getTensorBuffer().getDoubleList().reshape([1, 160, 160, 3]);
-    // Run inference
-    try {
-      _interpreter.run(input, output);
-    } catch (e) {
-      print("Error running inference: $e");
-    }
+    _interpreter.run(input, output);
 
-    print(
-        "Output buffer: ${_interpreter.getInputTensor(0).shape} okk1 ${_interpreter.getOutputTensor(0).shape}");
-
-    // Return the embedding vector
-    // return outputBuffer.reshape([128]).cast<double>();
-    // return outputBuffer.getDoubleList();
-    print("Output bufferlklkj: ${output[0].length}");
     return output[0].cast<double>();
   }
 
   TensorImage _preprocess(InputImage inputImage) {
-    final inputSize = 160; // Match this with your model's input size
+    const inputSize = 160; // Match this with your model's input size
 
     TensorImage tensorImage = TensorImage.fromFile(File(inputImage.filePath!));
 
@@ -295,26 +214,22 @@ class _FaceRecognitionPageState extends State<FaceRecognitionPage> {
 
     tensorImage = imageProcessor.process(tensorImage);
     return tensorImage;
-    // input size: [1, 160, 160, 3]
   }
 
   String? _findClosestEmbedding(List<double> embedding) {
-    double minDistance = 0.3;
+    double minDistance = double.infinity;
     String? closestPerson;
 
     knownEmbeddings.forEach((knownEmbedding, personName) {
-      print("fkkkkk ${isTwoVectorsEqual(knownEmbedding, embedding)}");
       double distance = _euclideanDistance(embedding, knownEmbedding);
-      print("Distance: $distance");
       if (distance < minDistance) {
         minDistance = distance;
         closestPerson = personName;
       }
     });
 
-    // Define a threshold for recognition
-    if (minDistance < 1.0) {
-      // Adjust this threshold as needed
+    print("Distance: $minDistance");
+    if (minDistance < 0.019) {
       return closestPerson;
     } else {
       return null;
@@ -329,6 +244,26 @@ class _FaceRecognitionPageState extends State<FaceRecognitionPage> {
     return sqrt(sum);
   }
 
+  double _cosineSimilarity(List<double> vecA, List<double> vecB) {
+    double dotProduct = 0.0;
+    double magnitudeA = 0.0;
+    double magnitudeB = 0.0;
+
+    for (int i = 0; i < vecA.length; i++) {
+      dotProduct += vecA[i] * vecB[i];
+      magnitudeA += vecA[i] * vecA[i];
+      magnitudeB += vecB[i] * vecB[i];
+    }
+
+    magnitudeA = sqrt(magnitudeA);
+    magnitudeB = sqrt(magnitudeB);
+
+    if (magnitudeA == 0 || magnitudeB == 0) return 0.0;
+
+    final similarity = dotProduct / (magnitudeA * magnitudeB);
+    return 1 - similarity;
+  }
+
   Future<void> _showTaggingDialog(List<double> embedding) async {
     String? tagName;
 
@@ -336,12 +271,12 @@ class _FaceRecognitionPageState extends State<FaceRecognitionPage> {
       context: context,
       builder: (context) {
         return AlertDialog(
-          title: Text("Tag this face"),
+          title: const Text("Tag this face"),
           content: TextField(
             onChanged: (value) {
               tagName = value;
             },
-            decoration: InputDecoration(hintText: "Enter name"),
+            decoration: const InputDecoration(hintText: "Enter name"),
           ),
           actions: [
             TextButton(
@@ -353,7 +288,7 @@ class _FaceRecognitionPageState extends State<FaceRecognitionPage> {
                 }
                 Navigator.of(context).pop();
               },
-              child: Text("Tag"),
+              child: const Text("Tag"),
             ),
           ],
         );
@@ -365,19 +300,19 @@ class _FaceRecognitionPageState extends State<FaceRecognitionPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Face Recognition App"),
+        title: const Text("Face Recognition App"),
       ),
       body: ListView(
         children: [
           _image != null
               ? Image.file(_image!)
               : Container(height: 200, color: Colors.grey[300]),
-          SizedBox(height: 20),
+          const SizedBox(height: 20),
           ElevatedButton(
             onPressed: _getImage,
-            child: Text("Select Image"),
+            child: const Text("Select Image"),
           ),
-          SizedBox(height: 20),
+          const SizedBox(height: 20),
           faces.isNotEmpty
               ? Container(
                   height: 400,
